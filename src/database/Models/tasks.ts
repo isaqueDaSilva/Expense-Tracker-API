0ca028e934cc735ea735de4a3c7fdc5d9174ccbc
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-09-21 02:33:29 -0300

    fix - Adding checkers to title and description rows

--- src/database/Models/tasks.ts
+++ src/database/Models/tasks.ts
@@ -1,8 +1,8 @@
 export const tasksTableSchema = `
     CREATE TABLE IF NOT EXISTS tasks (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid() UNIQUE NOT NULL,
-        title VARCHAR(100) NOT NULL,
-        description TEXT,
+        title VARCHAR(100) NOT NULL CHECK (char_length(title) <= 100),
+        description TEXT CHECK (char_length(description) <= 500),
         value NUMERIC(10, 2) NOT NULL CHECK(value >= 0.1),
         date DATE,
         category UUID REFERENCES categories(id) ON DELETE SET NULL,
