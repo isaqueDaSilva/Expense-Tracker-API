a22f9e5e6b6cee1ffb505edc591f4a3010518596
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-09-20 22:25:09 -0300

    feat - creating a handler to read authenticationHandler
    This handler is used to get the username and password from the http header, when a sign in process will be requested to be performed.

---
+++ src/server/Controllers/Services/basicAuthenticantionHandler.ts
@@ -0,0 +1,24 @@
+import type { IncomingMessage } from "http";
+
+export function basicAuthenticationHandler(request: IncomingMessage): { username: string; password: string } {
+    const authHeader = request.headers['authorization'];
+
+    if (!authHeader || !authHeader.startsWith('Basic ')) {
+        throw new Error("Missing or invalid Authorization header");
+    }
+
+    const base64Credentials = authHeader.split(' ')[1];
+
+    if (typeof base64Credentials === 'string') {
+        const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
+        const [username, password] = credentials.split(':');
+
+        if (!username || !password) {
+            throw new Error("Invalid Authorization header format");
+        }
+
+        return { username, password };
+    } else {
+        throw new Error("Invalid Authorization header format");
+    }
+}
