80ecfa20c0fb35d4a516137b4c41078cb08dce45
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-09-24 00:19:36 -0300

    refactor - switching from the manual set response to a function that handles with it.

--- src/server/Controllers/authenticationController.ts
+++ src/server/Controllers/authenticationController.ts
@@ -3,33 +3,31 @@ import { decodeJSONBody } from "./services/jsonDecoder";
 import { decodeCreateUserDTO } from "./services/user/decodeUser";
 import { hashPassword, comparePassword } from "./services/passwordHash";
 import { createUser, getUserByEmail, updateUserLoginStatus, deleteUser } from "./services/user/userCRUD";
-import { createJWT, verifyJWT, jwtAuthenticationHandler } from "./services/jwtService";
+import { createJWT } from "./services/jwtService";
 import { basicAuthenticationHandler } from "./services/basicAuthenticantionHandler";
 import { disableToken } from "./services/tokens/tokenCRUD";
+import { setResponse } from "./services/setResponse";
+import { verifyToken } from "./services/verifyToken";
 
 export async function signup(request: IncomingMessage, response: ServerResponse) {
     try {
         const newUser = await decodeJSONBody(request, decodeCreateUserDTO);
         const hashedPassword = await hashPassword(newUser.password);
-        newUser.password = "" // Clear the plain password from memory as soon as possible
+        newUser.password = ""; // Clear the plain password from memory as soon as possible
 
         const createdUser = await createUser(newUser.email, newUser.username, hashedPassword);
 
-        const token = createJWT(createdUser.id)
+        const token = createJWT(createdUser.id);
 
         const responseJSON = {
             token: token,
             user: createdUser
         };
 
-        response.statusCode = 201;
-        response.setHeader('Content-Type', 'application/json');
-        response.end(JSON.stringify(responseJSON));
+        setResponse(response, 201, "result", responseJSON);
     } catch (error) {
         console.error("Error processing signup:", error);
-        response.statusCode = 400;
-        response.setHeader('Content-Type', 'application/json');
-        response.end(JSON.stringify({ error: "Internal Server Error" }));
+        setResponse(response, 500, "error", "Internal Server Error.");
     }
 };
 
@@ -53,55 +51,38 @@ export async function signin(request: IncomingMessage, response: ServerResponse)
                 user: user.userResponse
             };
 
-            response.statusCode = 200;
-            response.setHeader('Content-Type', 'application/json');
-            response.end(JSON.stringify(responseJSON));
+            setResponse(response, 200, "result", responseJSON);
             return;
+        } else {
+            setResponse(response, 401, "Error", "Authentication failed.");
         }
 
     } catch (error) {
         console.error("Error processing login:", error);
-        response.statusCode = 401;
-        response.setHeader('Content-Type', 'application/json');
-        response.end(JSON.stringify({ error: "Internal Server Error" }));
+        setResponse(response, 500, "error", "Internal Server Error.");
     }
 };
 
-export function verifyToken(request: IncomingMessage): {value: string, userID: string} {
-    const token = jwtAuthenticationHandler(request);
-    const userID = verifyJWT(token);
-
-    return {value: token, userID: userID};
-}
-
 export async function signout(request: IncomingMessage, response: ServerResponse) {
     try {
-        const token = verifyToken(request)
+        const token = await verifyToken(request)
         await updateUserLoginStatus(token.userID, false);
         await disableToken(token.value); // Store the token in the disabled tokens list
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'application/json');
-        response.end(JSON.stringify({ message: "User signed out successfully" }));
+        setResponse(response, 204, "message", "User signed out successfully" );
     } catch (error) {
         console.error("Error processing signout:", error);
-        response.statusCode = 401;
-        response.setHeader('Content-Type', 'application/json');
-        response.end(JSON.stringify({ error: "Internal Server Error" }));
+        setResponse(response, 500, "error", "Internal Server Error.");
     }
 };
 
 export async function removeUser(request: IncomingMessage, response: ServerResponse) {
     try {
-        const token = verifyToken(request);
+        const token = await verifyToken(request);
         await deleteUser(token.userID);
         await disableToken(token.value); // Store the token in the disabled tokens list
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'application/json');
-        response.end(JSON.stringify({ message: "User deleted successfully" }));
+        setResponse(response, 204, "message", "User deleted successfully" );
     } catch (error) {
         console.error("Error processing user deletion:", error);
-        response.statusCode = 401;
-        response.setHeader('Content-Type', 'application/json');
-        response.end(JSON.stringify({ error: "Internal Server Error" }));
+        setResponse(response, 500, "error", "Internal Server Error.");
     }
-}
+};
