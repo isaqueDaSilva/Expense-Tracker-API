afc10aac32347bd52abf8248fd43099210471e1c
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-09-24 00:23:23 -0300

    feat - adding a decoder method to decode UpdateTaskDTO from requests.

--- src/server/Controllers/Services/tasks/decodeTaskDTO.ts
+++ src/server/Controllers/Services/tasks/decodeTaskDTO.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import type { CreateTaskDTO } from '../../dto/taskDTO';
+import type { CreateTaskDTO, UpdateTaskDTO } from '../../dto/taskDTO';
 
 export function decodeCreateTaskDTO(jsonString: string): CreateTaskDTO {
     const parsed = JSON.parse(jsonString, (key, value) => {
@@ -25,6 +25,34 @@ export function decodeCreateTaskDTO(jsonString: string): CreateTaskDTO {
         date: z.string().refine((date) => !isNaN(Date.parse(date)), {
             message: "Invalid date format"
         }),
+        category: z.uuid("Invalid category ID").optional()
+    }).parse(parsed);
+};
+
+export function decodeUpdateTaskDTO(jsonString: string): UpdateTaskDTO {
+    const parsed = JSON.parse(jsonString, (key, value) => {
+        if (key === 'title' || key === 'description' || key === 'date' || key === 'category') {
+            if (typeof value !== 'string') {
+                throw new Error(`Invalid type for ${key}: expected string`);
+            }
+            return value;
+        }
+        if (key === 'value') {
+            if (typeof value !== 'number') {
+                throw new Error(`Invalid type for ${key}: expected number`);
+            }
+            return value;
+        }
+        return value;
+    });
+
+    return z.object({
+        title: z.string().min(1, "Title cannot be empty").max(100, "Title must be at most 100 characters long").optional(),
+        description: z.string().max(500, "Description must be at most 500 characters long").optional(),
+        value: z.number().min(0.1, "Value must be at least 0.1").optional(),
+        date: z.string().refine((date) => !isNaN(Date.parse(date)), {
+            message: "Invalid date format"
+        }).optional(),
         category: z.uuid("Invalid category ID").optional()
     }).parse(parsed);
 }
