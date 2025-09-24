e150b61d4119746b0a5327433734ca33637bf454
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-09-24 00:24:41 -0300

    feat - adding a method to check if has a token with that raw value stored at the database,

--- src/server/Controllers/Services/Tokens/tokenCRUD.ts
+++ src/server/Controllers/Services/Tokens/tokenCRUD.ts
@@ -1,10 +1,26 @@
 import { database } from "../../../../app";
 
-export async function disableToken(token: string): Promise<void> {
+export async function disableToken(token: string) {
     const newToken = `
         INSERT INTO disabled_tokens (raw_token_value)
         VALUES ($1);
     `;
     const values = [token];
     await database.query(newToken, values);
+};
+
+export async function isTokenValid(rawToken: string): Promise<boolean> {
+    const getToken = `
+        SELECT COUNT(raw_token_value) FROM disabled_tokens
+        WHERE raw_token_value = $1
+    `;
+
+    const values = [rawToken]
+    const result = await database.query(getToken, values)
+
+    if (result.length == 0) {
+        return true
+    } else {
+        return false
+    }
 };
