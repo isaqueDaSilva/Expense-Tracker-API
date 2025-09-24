74a1d6e4931e5eda27d779a03b80a459dbccaeba
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-09-24 00:31:17 -0300

    feat - creating a decoder to decode category json from request.

---
+++ src/server/Controllers/Services/category/decodeCategoryDTO.ts
@@ -0,0 +1,34 @@
+import { json, z } from 'zod'
+import type { CategoryDTO } from '../../dto/categoryDTO';
+
+function decodeCategoryDTO(jsonString: string, isCreation: boolean): CategoryDTO {
+    const parsed = JSON.parse(jsonString, (key, value) => {
+        if (key === 'title') {
+            if (typeof value !== 'string') {
+                throw new Error(`Invalid type for ${key}: Expected string`)
+            }
+
+            return value
+        }
+
+        return value;
+    });
+
+    if (isCreation) {
+        return z.object({
+            title: z.string().min(1, "Title cannot be empty").max(100, "Title must be at most 100 characters long")
+        }).parse(parsed);
+    } else {
+        return z.object({
+            title: z.string().min(1, "Title cannot be empty").max(100, "Title must be at most 100 characters long").optional()
+        }).parse(parsed);
+    };
+};
+
+export function decodeCreateCategoryDTO(jsonString: string): CategoryDTO {
+    return decodeCategoryDTO(jsonString, true)
+};
+
+export function decodeUpdateCategoryDTO(jsonString: string): CategoryDTO {
+    return decodeCategoryDTO(jsonString, false)
+}
