5916ff10a2dbe3cc4070a1fef555d308ec3b8426
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-09-24 00:28:33 -0300

    feat - creating a function to check if the given token is valid.

---
+++ src/server/Controllers/Services/verifyToken.ts
@@ -0,0 +1,20 @@
+import type { IncomingMessage } from "http";
+import { jwtAuthenticationHandler, verifyJWT } from "./jwtService";
+import { disableToken, isTokenValid } from "./tokens/tokenCRUD";
+
+export async function verifyToken(request: IncomingMessage): Promise<{value: string, userID: string}> {
+    const token = jwtAuthenticationHandler(request);
+    try {
+        const userID = verifyJWT(token);
+
+        if (await isTokenValid(token)) {
+            return {value: token, userID: userID};
+        } else {
+            await disableToken(token)
+            throw new Error("Invalid token.")
+        }
+    } catch {
+        await disableToken(token)
+        throw new Error("Invalid token.")
+    }
+};
