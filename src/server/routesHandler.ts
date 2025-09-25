c544bd05d155ae5a2d449cf644b0fe363aaf92d2
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-09-25 11:49:43 -0300

    feat - creating a route handler.
    This class is responsible to register all routes of the API, and when a request coming in, we check if the given route matches as one inside of the handler.

---
+++ src/server/routesHandler.ts
@@ -0,0 +1,53 @@
+import { IncomingMessage, ServerResponse } from "http";
+
+type RouteHandler = (req: IncomingMessage, res: ServerResponse) => void;
+
+export interface Route {
+  method: string;
+  path: string;
+  handler: RouteHandler,
+};
+
+export class RoutesHandler {
+    private static shared: RoutesHandler;
+
+    private constructor() {}
+
+    public static getSharedInstance(): RoutesHandler {
+        if (!RoutesHandler.shared) {
+            // Create a new instance only if one doesn't exist
+            RoutesHandler.shared = new RoutesHandler();
+        }
+
+        return RoutesHandler.shared; // Return the single instance
+    }
+
+    private routes: Route[] = [];
+
+    addRoutes(route: Route) {
+        this.routes.push(route)
+    };
+
+    getMatchedRoute(method: string, url: string): {handler: RouteHandler, params: Record<string, string>} | null {
+        for (const route of this.routes) {
+            if (route.method !== method) continue;
+
+            // Converte path com :params para regex
+            const paramNames: string[] = [];
+            const regexPath = route.path.replace(/:([^/]+)/g, (_, key) => {
+                paramNames.push(key);
+                return "([^/]+)";
+            });
+
+            const regex = new RegExp(`^${regexPath}$`);
+            const match = url.match(regex);
+
+            if (match) {
+                const params: Record<string, string> = {};
+                paramNames.forEach((name, i) => (params[name] = match[i + 1]));
+                return { handler: route.handler, params };
+            }
+        }
+        return null;
+    };
+}
