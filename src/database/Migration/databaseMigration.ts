2c6d4daa3768fbe7d92827201d882890f22690da
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-09-26 23:54:30 -0300

    refactor - adding all migration process, inside of a database transaction.

--- src/database/Migration/databaseMigration.ts
+++ src/database/Migration/databaseMigration.ts
@@ -3,20 +3,28 @@ import { usersTableSchema } from "../models/user.js";
 import { tasksTableSchema } from "../models/tasks.js";
 import { categoryTableSchema, groceries, leisure, electronics, utilities, clothing, health } from "../models/category.js";
 import { disabledTokensTableSchema } from "../models/disabledTokens.js";
-import { database } from "../../app.js";
+import { databaseConfig } from "../configuration/databaseConfig.js";
+import * as dotenv from 'dotenv';
 
-export async function databaseMigration(database: NeonQueryFunction<false, false>) {
-    await database.query(usersTableSchema);
-    await database.query(categoryTableSchema);
-    await database.query(groceries);
-    await database.query(leisure);
-    await database.query(electronics);
-    await database.query(utilities);
-    await database.query(clothing);
-    await database.query(health);
-    await database.query(tasksTableSchema);
-    await database.query(disabledTokensTableSchema);
+dotenv.config();
+
+async function databaseMigration(database: NeonQueryFunction<false, false>) {
+    await database.transaction(
+        [
+            database.query(usersTableSchema),
+            database.query(categoryTableSchema),
+            database.query(groceries),
+            database.query(leisure),
+            database.query(electronics),
+            database.query(utilities),
+            database.query(clothing),
+            database.query(health),
+            database.query(tasksTableSchema),
+            database.query(disabledTokensTableSchema)
+        ]
+    )
 };
 
 // Run migration
-databaseMigration(database);
+const database = databaseConfig();
+await databaseMigration(database);
