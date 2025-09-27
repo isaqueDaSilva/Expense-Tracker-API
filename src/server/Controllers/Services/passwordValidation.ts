f48bbe37c2a9126234aabb805d6265885edc8c7d
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-09-27 00:00:31 -0300

    fix - correcting the regex
    This regex now analyse if the give password contain at least one uppercase letter, one lowercase letter, one number, and one special character.

--- src/server/Controllers/Services/passwordValidation.ts
+++ src/server/Controllers/Services/passwordValidation.ts
@@ -1,4 +1,4 @@
 export function passwordValidation(password: string): boolean {
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{8,}$/;
     return passwordRegex.test(password);
 }
