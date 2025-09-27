4ced3b6f2831dd0b7b7b2e3f15fad2324119a92c
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-09-26 23:55:26 -0300

    fix - repositioning some SQL commands that was in a wrong place.

--- src/database/Models/category.ts
+++ src/database/Models/category.ts
@@ -2,33 +2,33 @@ export const categoryTableSchema = `
     CREATE TABLE IF NOT EXISTS categories (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid() UNIQUE NOT NULL,
         title VARCHAR(30) NOT NULL,
-        created_by UUID REFERENCES users(id) ON DELETE SET NULL DEFAULT NULL,
-        created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        created_by UUID DEFAULT NULL REFERENCES users(id) ON DELETE SET NULL,
+        created_at DATE DEFAULT CURRENT_DATE NOT NULL
     );
 `;
 
 // MARK: DEFAULT CATEGORIES
 
 export const groceries = `
-    INSERT INTO categories (title) VALUES ('Groceries') ON CONFLICT (title) DO NOTHING;
+    INSERT INTO categories (title) VALUES ('Groceries');
 `;
 
 export const leisure = `
-    INSERT INTO categories (title) VALUES ('Leisure') ON CONFLICT (title) DO NOTHING;
+    INSERT INTO categories (title) VALUES ('Leisure');
 `;
 
 export const electronics = `
-    INSERT INTO categories (title) VALUES ('Electronics') ON CONFLICT (title) DO NOTHING;
+    INSERT INTO categories (title) VALUES ('Electronics');
 `;
 
 export const utilities = `
-    INSERT INTO categories (title) VALUES ('Utilities') ON CONFLICT (title) DO NOTHING;
+    INSERT INTO categories (title) VALUES ('Utilities');
 `;
 
 export const clothing = `
-    INSERT INTO categories (title) VALUES ('Clothing') ON CONFLICT (title) DO NOTHING;
+    INSERT INTO categories (title) VALUES ('Clothing');
 `;
 
 export const health = `
-    INSERT INTO categories (title) VALUES ('Health') ON CONFLICT (title) DO NOTHING;
+    INSERT INTO categories (title) VALUES ('Health');
 `;
