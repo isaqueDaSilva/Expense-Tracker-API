782c40a1cbef7856e27a6b26e65850cc4ec615c4
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-09-30 22:35:09 -0300

    fix - specifying the type of the token when we verify it.

--- src/server/Controllers/tasksController.ts
+++ src/server/Controllers/tasksController.ts
@@ -6,24 +6,25 @@ import { createTask, deleteTask, getAllTasks, getAllTasksByCategory, getTasksByD
 import { getParameterURL } from "./services/getParameterURL.js";
 import { isPageValid } from "./services/isPageValid.js";
 import { setResponse } from "./services/setResponse.js";
+import { TokenType } from "./services/tokens/tokenType.js";
 
 export async function createNewTask(request: IncomingMessage, response: ServerResponse) {
     try {
-        const token = await verifyToken(request);
+        const token = await verifyToken(request, TokenType.access);
         const newTask = await decodeJSONBody(request, decodeCreateTaskDTO);
 
         const task = await createTask(newTask, token.userID);
 
-        setResponse(response, 201, "task", task);
+        setResponse(response, 201, task);
     } catch (error) {
         console.error("Error processing task creation:", error);
-        setResponse(response, 500, "error", "Internal Server Error.");
+        setResponse(response, 500, { error: "Internal Server Error."} );
     }
 };
 
 export async function getTasks(request: IncomingMessage, response: ServerResponse) {
     try {
-        const token = await verifyToken(request);
+        const token = await verifyToken(request, TokenType.access);
         const pageParameter = getParameterURL(request, 'page');
         const page = pageParameter ? parseInt(pageParameter, 10) : 1;
 
@@ -32,22 +33,22 @@ export async function getTasks(request: IncomingMessage, response: ServerRespons
         }
 
         const tasks = await getAllTasks(token.userID, page);
-        setResponse(response, 200, "tasks", tasks);
+        setResponse(response, 200, tasks);
     } catch (error) {
         console.error("Error processing get tasks:", error);
-        setResponse(response, 500, "error", "Internal Server Error.");
+        setResponse(response, 500, { error: "Internal Server Error." } );
     }
 };
 
 export async function getTasksByCategory(request: IncomingMessage, response: ServerResponse) {
     try {
-        const token = await verifyToken(request);
+        const token = await verifyToken(request, TokenType.access);
         const categoryID = getParameterURL(request, 'category');
         const pageParameter = getParameterURL(request, 'page');
         const page = pageParameter ? parseInt(pageParameter, 10) : 1;
 
         if (!categoryID) {
-            setResponse(response, 400, "error", "Category ID is required.");
+            setResponse(response, 400, { error: "Category ID is required." } );
             return;
         }
 
@@ -56,23 +57,23 @@ export async function getTasksByCategory(request: IncomingMessage, response: Ser
         }
 
         const tasks = await getAllTasksByCategory(token.userID, categoryID, page);
-        setResponse(response, 200, "tasks", tasks);
+        setResponse(response, 200, tasks);
     } catch (error) {
         console.error("Error processing get tasks by category:", error);
-        setResponse(response, 500, "error", "Internal Server Error.");
+        setResponse(response, 500, { error: "Internal Server Error." } );
     }
 };
 
 export async function getAllTasksByDate(request: IncomingMessage, response: ServerResponse) {
     try {
-        const token = await verifyToken(request);
+        const token = await verifyToken(request, TokenType.access);
         const initialDate = getParameterURL(request, 'initial-date');
         const finalDate = getParameterURL(request, 'final-date');
         const pageParameter = getParameterURL(request, 'page');
         const page = pageParameter ? parseInt(pageParameter, 10) : 1;
 
         if ((!initialDate || !finalDate) || (initialDate == finalDate)) {
-            setResponse(response, 400, "error", "Date out of range.");
+            setResponse(response, 400, { error: "Date out of range." } );
             return;
         }
 
@@ -81,64 +82,64 @@ export async function getAllTasksByDate(request: IncomingMessage, response: Serv
         }
 
         const tasks = await getTasksByDate(token.userID, initialDate, finalDate, page);
-        setResponse(response, 200, "tasks", tasks);
+        setResponse(response, 200, tasks);
     } catch (error) {
         console.error("Error processing get tasks by date range:", error);
-        setResponse(response, 500, "error", "Internal Server Error.");
+        setResponse(response, 500, { error: "Internal Server Error." } );
     }
 }
 
 export async function getTask(request: IncomingMessage, response: ServerResponse) {
     try {
-        const token = await verifyToken(request);
+        const token = await verifyToken(request, TokenType.access);
         const taskID = getParameterURL(request, 'id');
 
         if (!taskID) {
-            setResponse(response, 400, "error", "Task ID is required.");
+            setResponse(response, 400, { error: "Task ID is required." } );
             return;
         }
 
         const task = await getTaskByID(taskID, token.userID);
-        setResponse(response, 200, "task", task);
+        setResponse(response, 200, task);
     } catch (error) {
         console.error("Error processing get task by ID:", error);
-        setResponse(response, 500, "error", "Internal Server Error.");
+        setResponse(response, 500, { error: "Internal Server Error." } );
     }
 };
 
 export async function updateCurrentTask(request: IncomingMessage, response: ServerResponse) {
     try {
-        const token = await verifyToken(request);
+        const token = await verifyToken(request, TokenType.access);
         const taskID = getParameterURL(request, 'id');
 
         if (!taskID) {
-            setResponse(response, 400, "error", "Task ID is required.");
+            setResponse(response, 400, { error: "Task ID is required." } );
             return;
         }
 
         const updatedTask = await decodeJSONBody(request, decodeUpdateTaskDTO);
         const task = await updateTask(updatedTask, taskID, token.userID);
-        setResponse(response, 200, "task", task);
+        setResponse(response, 200, task);
     } catch (error) {
         console.error("Error processing task update:", error);
-        setResponse(response, 500, "error", "Internal Server Error");
+        setResponse(response, 500, { error: "Internal Server Error" } );
     }
 };
 
 export async function deleteCurrentTask(request: IncomingMessage, response: ServerResponse) {
     try {
-        const token = await verifyToken(request);
+        const token = await verifyToken(request, TokenType.access);
         const taskID = getParameterURL(request, 'id');
 
         if (!taskID) {
-            setResponse(response, 400, "error", "Task ID is required.");
+            setResponse(response, 400, { error: "Task ID is required." } );
             return;
         }
 
         await deleteTask(taskID, token.userID);
-        setResponse(response, 204, "message", "Task deleted with success.");
+        setResponse(response, 204, {message: "Task deleted with success."});
     } catch (error) {
         console.error("Error processing task deletion:", error);
-        setResponse(response, 500, "error", "Internal Server Error.");
+        setResponse(response, 500, {error: "Internal Server Error."});
     }
 };
