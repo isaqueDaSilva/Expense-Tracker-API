d77b99b20b83d90f67a128a87abe633beded50f6
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-09-30 22:37:44 -0300

    feat - enabling the jwt service to accept the refresh token, and also adding an extra verification process, to validate a jwt

--- src/server/Controllers/Services/jwtService.ts
+++ src/server/Controllers/Services/jwtService.ts
@@ -1,12 +1,14 @@
 import jwt from "jsonwebtoken";
 import type { IncomingMessage } from "http";
+import { TokenType } from "./tokens/tokenType.js";
+import { JWTPayload } from "./tokens/jwtPayload.js";
 
-export function createJWT(userID: string): string {
+export function createJWT(userID: string): {accessToken: string, refreshToken: string } {
     const issuedAt: Date = new Date();
     const randomNumber = Math.floor(Math.random() * 1000000).toString();
     const issuer = process.env.JWT_ISSUER || "Expense Tracker API";
 
-    const payload = {
+    const accessTokenPayload: JWTPayload = {
         id: userID + randomNumber + issuedAt.getTime().toString(),
         issuer: issuer,
         subject: userID,
@@ -14,21 +16,48 @@ export function createJWT(userID: string): string {
         expiration: new Date(issuedAt.getTime() + 60 * 60 * 1000).toString() // 1 hour
     }
 
-    const secret = process.env.JWT_SECRET;
+    const refreshTokenPayload: JWTPayload = {
+        id: userID + randomNumber + issuedAt.getTime().toString(),
+        issuer: issuer,
+        subject: userID,
+        issuedAt: issuedAt.toString(),
+        expiration: new Date(issuedAt.getTime() + 60 * 60 * 1000 * 24 * 7).toString() // 7 days
+    }
 
-    if (typeof secret === "string") {
-        return jwt.sign(payload, secret);
+    const accessTokenSecret = process.env.JWT_ACCESS_SECRET;
+    const refreshTokenSecret = process.env.JWT_REFRESH_SECRET;
+
+    if (typeof accessTokenSecret === "string" && refreshTokenSecret === "string") {
+        const accessToken = jwt.sign(accessTokenPayload, accessTokenSecret);
+        const refreshToken = jwt.sign(refreshTokenPayload, refreshTokenSecret);
+
+        return { accessToken, refreshToken }
     } else {
         throw new Error("Internal server error.");
     }
 };
 
-export function verifyJWT(token: string): string {
-    const secret = process.env.JWT_SECRET;
+export function verifyJWT(token: string, tokenType: TokenType): string {
+    let secret: string | undefined;
+    let issuer = process.env.JWT_ISSUER
 
-    if (typeof secret === "string") {
+    switch (tokenType) {
+        case TokenType.access: 
+            secret = process.env.JWT_SECRET;
+            break;
+        case TokenType.refresh:
+            secret = process.env.JWT_REFRESH_SECRET;
+            break
+    }
+
+    if (typeof secret === "string" && typeof issuer === "string") {
         try {
-            const decoded = jwt.verify(token, secret) as { subject: string };
+            const decoded = jwt.verify(token, secret) as JWTPayload;
+
+            if (!(decoded.issuer === issuer)) {
+                throw new Error("Issuer not matches as the issuer value of this API.")
+            }
+
             return decoded.subject;
         } catch (error) {
             throw new Error("Invalid token");
@@ -38,8 +67,17 @@ export function verifyJWT(token: string): string {
     }
 };
 
-export function jwtAuthenticationHandler(request: IncomingMessage) {
-    const authHeader = request.headers['authorization'];
+export function getJWTValue(request: IncomingMessage, tokenType: TokenType): string {
+    let authHeader: string | undefined;
+
+    switch (tokenType) {
+        case TokenType.access:
+            authHeader = request.headers['authorization'];
+            break
+        case TokenType.refresh:
+            authHeader = request.headers['refreshtoken'] as string | undefined;
+            break
+    }
 
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
         throw new Error("Missing or invalid Authorization header");
