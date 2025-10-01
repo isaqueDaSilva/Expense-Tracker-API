12991bc05734891dd67c225f45851a2134352d75
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-09-30 22:38:18 -0300

    fix - specifying the type of the token when we verify it.

--- src/server/Controllers/Services/verifyToken.ts
+++ src/server/Controllers/Services/verifyToken.ts
@@ -1,16 +1,19 @@
 import type { IncomingMessage } from "http";
-import { jwtAuthenticationHandler, verifyJWT } from "./jwtService.js";
+import { getJWTValue, verifyJWT } from "./jwtService.js";
 import { disableToken, isTokenValid } from "./tokens/tokenCRUD.js";
+import { TokenType } from "./tokens/tokenType.js";
+import { updateUserLoginStatus } from "./user/userCRUD.js";
 
-export async function verifyToken(request: IncomingMessage): Promise<{value: string, userID: string}> {
-    const token = jwtAuthenticationHandler(request);
+export async function verifyToken(request: IncomingMessage, tokenType: TokenType): Promise<{value: string, userID: string}> {
+    const token = getJWTValue(request, tokenType);
     try {
-        const userID = verifyJWT(token);
+        const userID = verifyJWT(token, tokenType);
 
         if (await isTokenValid(token)) {
             return {value: token, userID: userID};
         } else {
             await disableToken(token)
+            await updateUserLoginStatus(userID, false)
             throw new Error("Invalid token.")
         }
     } catch {
