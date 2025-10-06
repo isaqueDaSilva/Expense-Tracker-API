587de9a5f238e8324f3e5efcbab772faf0d833c5
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-10-06 00:58:08 -0300

    fix - correcting the wrong column name the user_id to the correct created_by column.

--- src/server/Controllers/Services/category/categoryCRUD.ts
+++ src/server/Controllers/Services/category/categoryCRUD.ts
@@ -3,10 +3,10 @@ import type { ReadCategoryDTO } from "../../dto/categoryDTO.js";
 
 export async function createCategory(categoryTitle: string, userID: string): Promise<ReadCategoryDTO> {
     const newCategory = `
-        INSERT INTO categories (title) VALUES ($1) RETURNING id, title, created_at AS createdAt
+        INSERT INTO categories (title, created_by) VALUES ($1, $2) RETURNING id, title, created_at AS createdAt
     `;
 
-    const values = [categoryTitle];
+    const values = [categoryTitle, userID];
     const result = await database.query(newCategory, values);
 
     if (result.length == 1) {
@@ -28,15 +28,18 @@ export async function getAllCategories(userID: string, currentPage: number): Pro
         const offset = (currentPage - 1) * categoryPerPage;
 
         const getCategories = `
-            SELECT id, title, created_at AS createdAt
+            SELECT id, title, created_by, created_at AS createdAt
             FROM categories
-            WHERE user_id = $1 OR user_id = NULL
+            WHERE (created_by = $1 OR created_by IS NULL)
             ORDER BY created_at DESC
             LIMIT $2 OFFSET $3
         `;
 
         const values = [userID, categoryPerPage, offset];
         const result = await database.query(getCategories, values);
+
+        console.log("response", result)
+
         return result.map(row => row as ReadCategoryDTO);
     } else {
         throw new Error("Invalid page number. Page number must be 1 or greater.");
@@ -47,10 +50,10 @@ export async function getCategoryByID(userID: string, categoryID: string): Promi
     const getCategory = `
         SELECT id, title, created_at AS createdAt
         FROM categories
-        WHERE id = $2 AND user_id = $1
+        WHERE id = $1 AND created_by = $2
     `;
 
-    const values = [userID, categoryID];
+    const values = [categoryID, userID];
     const result = await database.query(getCategory, values);
 
     if (result.length == 1) {
@@ -70,7 +73,7 @@ export async function updateCategory(userID: string, categoryID: string, newTitl
     const updateCategory = `
         UPDATE categories
         SET title = $1
-        WHERE id = $2 AND user_id = $3
+        WHERE id = $2 AND created_by = $3
         RETURNING id, title, created_at AS createdAt
     `;
 
@@ -78,7 +81,7 @@ export async function updateCategory(userID: string, categoryID: string, newTitl
     const result = await database.query(updateCategory, values);
 
     if (result.length == 1) {
-        const updatedCategory = result [0];
+        const updatedCategory = result[0];
 
         if (typeof updatedCategory === "object") {
             return updatedCategory as ReadCategoryDTO;
@@ -93,7 +96,7 @@ export async function updateCategory(userID: string, categoryID: string, newTitl
 export async function deleteCategory(categoryID: string, userID: string) {
     const deleteCategory = `
         DELETE FROM categories
-        WHERE id = $1 AND user_id = $2
+        WHERE id = $1 AND created_by = $2
     `;
 
     const values = [categoryID, userID];
