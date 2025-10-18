0f4970212f0ee2f7d074ea5ede4f4e53240263c9
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-10-18 02:46:36 -0300

    feat - creating a new table to store users sessions.

---
+++ src/database/Models/sessions.ts
@@ -0,0 +1,11 @@
+export const sessionsTableSchema = `
+    CREATE TABLE IF NOT EXISTS sessions(
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid() UNIQUE NOT NULL,
+        user_id UUID REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+        at_verification_code TEXT UNIQUE NOT NULL,
+        refresh_token_id TEXT UNIQUE NOT NULL
+        refresh_token TEXT UNIQUE NOT NULL,
+        created_at DATE NOT NULL DEFAULT CURRENT_DATE,
+        updated_at DATE DEFAULT CURRENT_DATE NOT NULL
+    );
+`;
