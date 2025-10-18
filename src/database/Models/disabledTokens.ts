e4d2fd4945dfc246a7776ef42dc63b6e3d9a55d9
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-10-18 02:44:24 -0300

    remove - removing some files from the project

--- src/database/Models/disabledTokens.ts
+++
@@ -1,7 +0,0 @@
-export const disabledTokensTableSchema = `
-    CREATE TABLE IF NOT EXISTS disabled_tokens(
-        id UUID PRIMARY KEY DEFAULT gen_random_uuid() UNIQUE NOT NULL,
-        raw_token_value TEXT UNIQUE NOT NULL,
-        disabled_at DATE NOT NULL DEFAULT CURRENT_DATE
-    );
-`;
