c32fafac8ad525e202d244f1d4ef54d57394a05b
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-10-22 20:28:21 -0300

    cleanup - removing decode expense search method.

--- src/services/expenses/decodeExpenseDTO.ts
+++ src/services/expenses/decodeExpenseDTO.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import type { CreateExpenseDTO, ExpenseSearch, UpdateExpenseDTO } from '../../dtos/expenseDTO.js';
+import type { CreateExpenseDTO, UpdateExpenseDTO } from '../../dtos/expenseDTO.js';
 
 export function decodeCreateExpenseDTO(jsonString: string): CreateExpenseDTO {
     const parsed = JSON.parse(jsonString, (key, value) => {
@@ -56,26 +56,3 @@ export function decodeUpdateExpenseDTO(jsonString: string): UpdateExpenseDTO {
         category: z.uuid("Invalid category ID").optional()
     }).parse(parsed);
 };
-
-export function decodeExpenseSearch(jsonString: string): ExpenseSearch {
-    const parsed = JSON.parse(jsonString, (key, value) => {
-        if (key === 'initialDate' || key === 'finalDate') {
-            if (typeof value !== 'string') {
-                throw new Error(`Invalid type for ${key}: expected string`);
-            }
-
-            return value
-        }
-
-        return value
-    });
-
-    return z.object({
-        initialDate: z.string().refine((initialDate) => !isNaN(Date.parse(initialDate)), {
-            message: "Invalid date format"
-        }),
-        finalDate: z.string().refine((finalDate) => !isNaN(Date.parse(finalDate)), {
-            message: "Invalid date format"
-        })
-    }).parse(parsed);
-}
