6d05979d33a2aaae84f6e8f82fb00444e24085ba
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-10-28 14:56:34 -0300

    cleanup - removing the is_logged row and updating the type of last_logged_date row to get exactly date time that the user perform your last sigin at the system.

--- src/database/Models/user.ts
+++ src/database/Models/user.ts
@@ -4,8 +4,7 @@ export const usersTableSchema = `
         username VARCHAR(50) UNIQUE NOT NULL,
         email TEXT UNIQUE NOT NULL CHECK (email LIKE '%@%_%.%_%'),
         password_hash TEXT UNIQUE NOT NULL,
-        is_logged BOOLEAN NOT NULL DEFAULT true,
-        last_logged_date DATE DEFAULT CURRENT_DATE NOT NULL,
+        last_logged_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
         created_at DATE DEFAULT CURRENT_DATE NOT NULL
     );
 `;
