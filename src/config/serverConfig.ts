766af7891d7da50024d50197225c47817fb0a059
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-10-28 14:51:09 -0300

    cleanup - unnecessary code

--- src/config/serverConfig.ts
+++ src/config/serverConfig.ts
@@ -208,9 +208,8 @@ RoutesHandler.getSharedInstance().addRoutes({
 
 export const server = createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Max-Age', 86400);
   res.setHeader("Access-Control-Allow-Credentials", "true")
 
   if (req.method === 'OPTIONS') {
